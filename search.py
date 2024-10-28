dac7aadd78a0a1ff90658b85efc843fa043478d0
Henry Brown <henry.jy.brown@gmail.com>
2024-10-28 17:27:40 -0400

    imported data and created BFS algo

---
+++ search.py
@@ -0,0 +1,41 @@
+import json
+from typing import Union
+
+from collections import deque
+
+with open("hash-tables/tconst-to-nconst.json", "r") as f:
+    tconst_to_nconst = json.load(f)
+with open("hash-tables/nconst-to-tconst.json", "r") as f:
+    nconst_to_tconst = json.load(f)
+with open("hash-tables/tconst-to-movies.json", "r") as f:
+    tconst_to_movies = json.load(f)
+with open("hash-tables/nconst-to-people.json", "r") as f:
+    nconst_to_people = json.load(f)
+with open("hash-tables/movies-to-tconst.json", "r") as f:
+    movies_to_tconst = json.load(f)
+
+
+def find_path(start_movie: str, destination_movie: str) -> Union[None, list[str]]:
+    if start_movie == destination_movie:
+        return [start_movie]
+
+    visited = set()
+    queue = deque([(start_movie, [])])
+
+    while queue:
+        current, path = queue.popleft()
+        path = path + current
+
+        if current == destination_movie:
+            return path
+
+        if current in visited:
+            continue
+
+        visited.add(current)
+        for nconst in tconst_to_nconst.get(current, []):
+            for next_tconst in nconst_to_tconst.get(nconst, []):
+                if next_tconst not in visited:
+                    queue.append((next_tconst), path + [nconst])
+    # No path was found
+    return None
