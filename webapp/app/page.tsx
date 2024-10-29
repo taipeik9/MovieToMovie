44f97db315c8ace884d92e73ad4eccb19d89d975
Henry Brown <henry.jy.brown@gmail.com>
2024-10-29 00:49:15 -0400

    attempted creating first page, def not working yet

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,16 +1,26 @@
-import { Autocomplete, Container, Typography } from "@mui/material";
+"use client";
+import { Autocomplete, Container, TextField, Typography } from "@mui/material";
 
 export default async function Home() {
   const response = await fetch("http://0.0.0.0/movies", {
     cache: "no-store",
   });
-  const movieTitles = response.json();
+  const movieTitles = await response.json();
+
+  const renderInput = async (params: any) => {
+    return <TextField {...params} label="Movie" />;
+  };
 
   return (
     <Container>
       <Typography variant="h1">Movie to Movie Solver</Typography>
 
-      {/* <Autocomplete></Autocomplete> */}
+      <Autocomplete
+        disablePortal
+        options={movieTitles.detail}
+        sx={{ width: 300 }}
+        renderInput={renderInput}
+      />
     </Container>
   );
 }
