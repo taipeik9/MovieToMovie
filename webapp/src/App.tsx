30063769a3aa54044f8d3adde1233acda178c25e
Henry Brown <henry.jy.brown@gmail.com>
2024-10-30 15:47:59 -0400

    switched over to vite setup

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,44 +1,10 @@
-import React, { useEffect, useState } from "react";
-import Select from "react-select";
+import { useState } from "react";
 import "./App.css";
 
-type MoviesResponse = { detail: string[] };
-
-const createOptions = (movies: string[]) => {
-  return movies.map((movie: string) => {
-    return { value: movie, label: movie };
-  });
-};
-
 function App() {
-  const [movies, setMovies] = useState<string[] | null>(null);
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const loadMovies = async () => {
-      const response = await fetch("http://0.0.0.0/movies");
-      const movies: MoviesResponse = await response.json();
-      setMovies(movies.detail);
-    };
-    loadMovies();
-  }, []);
-
-  return (
-    <div className="wrapper">
-      <div className="container">
-        <h1>Movie to Movie</h1>
-        {movies && (
-          <>
-            <input list="data" />
-            <datalist>
-              {movies.map((movie) => (
-                <option key={movie}>{movie}</option>
-              ))}
-            </datalist>
-          </>
-        )}
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
