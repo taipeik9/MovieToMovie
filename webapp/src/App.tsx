146e49d2c5dfe43e9bae6d0fcaca7c071eb755e2
Henry Brown <henry.jy.brown@gmail.com>
2024-11-13 18:33:53 -0500

    fetching movies

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,14 +1,29 @@
 import { SearchBar } from "./components/SearchBar";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [results, setResults] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch("http://0.0.0.0:80/movies");
+      const movies = await response.json();
+
+      setMovies(movies.detail);
+      console.log(movies.detail.length);
+    };
+
+    fetchMovies();
+  }, []);
 
   return (
     <div className="App">
       <div className="search-bar-container">
-        <SearchBar setResults={setResults} />
+        {movies.length > 0 && (
+          <SearchBar setResults={setResults} movies={movies} />
+        )}
         <div>Search Results</div>
       </div>
     </div>
