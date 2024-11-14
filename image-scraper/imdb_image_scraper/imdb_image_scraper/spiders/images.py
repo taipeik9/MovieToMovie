96c5d3c438baec30291e5c08f590aab6deed8e8e
Henry Brown <henry.jy.brown@gmail.com>
2024-11-14 15:07:59 -0500

    first try at image scraper

--- image-scraper/imdb_image_scraper/imdb_image_scraper/spiders/images.py
+++ image-scraper/imdb_image_scraper/imdb_image_scraper/spiders/images.py
@@ -10,12 +10,30 @@ class ImagesSpider(scrapy.Spider):
         with open("graph.json", "r") as f:
             graph = json.load(f)
             ids = list(graph.keys())
-        for id in ids[:5]:
-            if id[:1] == "tt":
-                url = f"https://www.imdb.com/title/{id}"
-            elif id[:1] == "nm":
-                url = f"https://www.imdb.com/name/{id}"
-            yield scrapy.Request(url=url, callback=self.parse)
+        for id in ids:
+            if id[:2] == "tt":
+                url = f"https://www.imdb.com/title/{id}/"
+            elif id[:2] == "nm":
+                url = f"https://www.imdb.com/name/{id}/"
+            else:
+                continue
+            yield scrapy.Request(url=url, callback=self.parse, meta={"id": id})
 
     def parse(self, response):
-        pass
+        image_link = response.xpath(
+            '//div[@class="ipc-poster ipc-poster--baseAlt ipc-poster--media-radius'
+            ' ipc-poster--dynamic-width ipc-sub-grid-item ipc-sub-grid-item--span-2"]'
+            '/a[@class="ipc-lockup-overlay ipc-focusable"]/@href'
+        ).get()
+        if image_link:
+            yield response.follow(
+                url=image_link, callback=self.parse_image, meta=response.request.meta
+            )
+        else:
+            yield {"id": response.request.meta["id"], "url": None}
+
+    def parse_image(self, response):
+        data_image_id = response.request.url.split("/")[-2]
+        url = response.xpath(f'//img[@data-image-id="{data_image_id}-curr"]/@src').get()
+
+        yield {"id": response.request.meta["id"], "url": url}
