d23ceabecda05d7e868b201f0da576396c76b5db
Henry Brown <henry.jy.brown@gmail.com>
2024-11-13 21:47:20 -0500

    moved search bars to separate component

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,43 +1,10 @@
-import { SearchBar } from "./components/SearchBar";
 import "./App.css";
-import { useEffect, useState } from "react";
-import { SearchList } from "./components/SearchList";
+import { SearchBars } from "./SearchBars";
 
 function App() {
-  const [results, setResults] = useState([]);
-  const [movies, setMovies] = useState([]);
-  const [selection, setSelection] = useState("");
-  const [clear, setClear] = useState(false);
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await fetch("http://0.0.0.0:80/movies");
-      const moviesObj = await response.json();
-
-      setMovies(moviesObj.detail);
-    };
-
-    fetchMovies();
-  }, []);
-
   return (
     <div className="App">
-      <div className="search-bar-container">
-        {movies.length > 0 && (
-          <SearchBar
-            setResults={setResults}
-            setClear={setClear}
-            movies={movies}
-            clear={clear}
-          />
-        )}
-        <SearchList
-          movies={results}
-          setSelection={setSelection}
-          setClear={setClear}
-        />
-        <div>{selection}</div>
-      </div>
+      <SearchBars />
     </div>
   );
 }
