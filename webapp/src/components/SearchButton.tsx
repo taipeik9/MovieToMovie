35308ad354a5b100abf05ccdf751fd319798c9c2
Henry Brown <henry.jy.brown@gmail.com>
2024-11-16 11:59:52 -0500

    removed wrapper

--- webapp/src/components/SearchButton.tsx
+++ webapp/src/components/SearchButton.tsx
@@ -2,10 +2,8 @@ import "./SearchButton.css";
 
 export const SearchButton = ({ onClick }: { onClick: Function }) => {
   return (
-    <div>
-      <button className="search-button" onClick={() => onClick()}>
-        Search
-      </button>
-    </div>
+    <button className="search-button" onClick={() => onClick()}>
+      Search
+    </button>
   );
 };
